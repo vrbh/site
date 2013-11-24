b05a6b5e6518b263709039bde88df3a463cf18e1
paulsohier <paul999@phpbb.com>
2013-11-24 13:56:50 +0100

    Some site updates

--- src/Vrbh/SiteBundle/Controller/DefaultController.php
+++ src/Vrbh/SiteBundle/Controller/DefaultController.php
@@ -11,17 +11,54 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
+
+    private function createResponse()
     {
         $response = new Response();
 
-        $response->setPublic();
+        $user = $this->container->get('security.context')->getToken()->getUser();
 
-        $response->setSharedMaxAge(600);
+        if (!$user)
+        {
+            $response->setPublic();
+            $response->setSharedMaxAge(600);
+        }
+        else{
+            $response->setPrivate();
+            $response->setMaxAge(600);
+        }
+        return $response;
+    }
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return $this->render('VrbhSiteBundle:Default:index.html.twig', array(
+            'jumbo' => true,
+            'jumbo_url'     => $this->get('router')->generate('about'),
+            'jumbo_title'   => 'Trying to get your stock in control?',
+            'jumbo_text'    => 'Stock information helps controlling your stock and visalizing the available stock. With apps for iOS and Android everyone can simple check and register stock information. Based on the information new products can be ordered.',
+        ), $this->createResponse());
+    }
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutUrlAction()
+    {
+        return $this->render('VrbhSiteBundle:Default:about.html.twig', array(), $this->CreateResponse());
+    }
 
-        return $this->render('VrbhSiteBundle:Default:index.html.twig', array(), $response);
+    /**
+     * @Route("/sidebar/org", name="OrgSidebar")
+     */
+    public function orgSidebarAction()
+    {
+        $response = new Response();
+        $response->setPrivate(true);
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        return $this->render('VrbhSiteBundle:Default:orgSidebar.html.twig', array('orgs' => $user->getOrgs()), $response);
     }
 }
