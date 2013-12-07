7b6c84256d8244eaa111bc42ef07c03700682c85
Paul Sohier <paul999@phpbb.com>
2013-12-07 18:40:28 +0100

    Move some JS around.

--- src/Vrbh/SiteBundle/Resources/public/org.js
+++ src/Vrbh/SiteBundle/Resources/public/org.js
@@ -1,3 +1,32 @@
-/**
- * Created by paul_000 on 7-12-13.
- */
+$(document).ready(function() {
+    $('[data-save-state=create-new-org]').click(function() {
+
+        $('[data-save-state=create-new-org]').attr('disabled', 'disabled');
+        $('[data-save-state=create-new-org-cancel]').attr('disabled', 'disabled');
+
+        removeDisabled = function()
+        {
+            $('[data-save-state=create-new-org]').removeAttr('disabled');
+            $('[data-save-state=create-new-org-cancel]').removeAttr('disabled');
+        }
+
+        hideCreateNewOrg = function()
+        {
+            $('#createNewOrg').modal('hide');
+
+            removeDisabled();
+        }
+
+        var name = $('[data-save-state=create-new-org-name]').val();
+
+        if (!name)
+        {
+
+            removeDisabled();
+        }
+        else
+        {
+
+        }
+    });
+});
